bfad09496a5768037a54a4813dc3d8b61ce3ffac
Alex100688 <alessiozingaro2@gmail.com>
2022-10-19 15:30:52 +0200

    Aggiunge la concatenazione con "classAccount"in js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,7 @@ const lastName=prompt ("Inserisci il tuo cognome");
 console.log (lastName);
 //Inserisci il tuo colore preferito//
 const favoriteColor=prompt("Iserisci il tuo colore preferito");
+console.log (favoriteColor);
+//Inserisci le variabili più il #76//
+const classAccount= `${firstName} ${lastName} ${favoriteColor} 76`
+console.log (classAccount);
