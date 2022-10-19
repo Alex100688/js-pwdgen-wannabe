335f4b777081b4c03a80d6dac41b961713747ba3
Alex100688 <alessiozingaro2@gmail.com>
2022-10-19 15:20:10 +0200

    Aggiunge il const "lastName" al foglio js.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 //inserisci il tuo nome//
-const firstName=prompt( "Inserisci il tuo nome");
+const firstName=prompt ( "Inserisci il tuo nome");
 console.log (firstName);
+//Inserisci il tuo cognome//
+const lastName=prompt ("Inserisci il tuo cognome");
+console.log (lastName);
+//Inserisci il tuo colore preferito//
+const favoriteColor=prompt("Iserisci il tuo colore preferito");
